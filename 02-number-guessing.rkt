19bc3d47ac66da8d854b21eaddbdbbe4a2e96ac3
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-03-16 23:36:54 -0700

    02: Add start function

--- 02-number-guessing.rkt
+++ 02-number-guessing.rkt
@@ -2,8 +2,18 @@
 
 (define lower 1)
 (define upper 100)
+
+(define (start m n)
+  (set! lower (min m n))
+  (set! upper (max m n)))
+
 (define (guess)
   (quotient (+ lower upper) 2))
+
 (define (smaller)
   (set! upper (max lower (sub1 (guess))))
+  (guess))
+
+(define (bigger)
+  (set! lower (min upper (add1 (guess))))
   (guess))
